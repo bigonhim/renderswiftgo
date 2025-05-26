261153b982f0da730a8bddf0679959a7dfb0a959
Biegon <156933863+Bigoncode@users.noreply.github.com>
2025-05-27 00:10:36 +0300

    initial commit

--- swiftgo/settings.py
+++ swiftgo/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9nekow9v8auv9ji0fn&&#!8%d60&e**v(o_u!xsng%vo5c-9u6'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
